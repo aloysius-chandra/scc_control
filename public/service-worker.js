567c9d60893b100cd7e7a58bfbf5edf98cb21364
Aloysius Chandra <aloysiuschandra3@gmail.com>
2025-01-08 15:15:15 +0700

    fixing the redirect again

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,12 +20,10 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return (
-        cachedResponse || fetch(event.request).catch(() => caches.match("/"))
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
