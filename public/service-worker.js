70749ac8c6442addd11bc4d063ce53751aa0047b
Aloysius Chandra <aloysiuschandra3@gmail.com>
2025-01-08 11:40:10 +0700

    set up all things getting ready for PWA deployment

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,32 @@
+const CACHE_NAME = "scc-control-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/address_a.html",
+  "/address_b.html",
+  "/manifest.json",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+  "/screenshot1.png",
+  "/screenshot2.png"
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        "/index.html",
-        "/manifest.json",
-        "/icon-192x192.png",
-        "/icon-512x512.png",
-        "/screenshot1.png",
-        "/screenshot2.png"
-      ]);
-    })
+      caches.open(CACHE_NAME).then((cache) => {
+          return cache.addAll(urlsToCache).then(() => {
+              console.log("All resources cached successfully!");
+          }).catch((err) => {
+              console.error("Resource failed to cache:", err);
+          });
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      })
   );
 });
