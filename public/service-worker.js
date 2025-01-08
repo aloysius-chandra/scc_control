bcd496234a4ebc8629f97d7b4c13494ee056068d
Aloysius Chandra <aloysiuschandra3@gmail.com>
2025-01-08 13:37:09 +0700

    fixing the service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "scc-control-v1";
+const CACHE_NAME = "scc-control-cache-v1";
 const urlsToCache = [
-  "/",
+  "/", // alias ke index.html
   "/index.html",
   "/address_a.html",
   "/address_b.html",
@@ -8,25 +8,39 @@ const urlsToCache = [
   "/icon-192x192.png",
   "/icon-512x512.png",
   "/screenshot1.png",
-  "/screenshot2.png"
+  "/screenshot2.png",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-          return cache.addAll(urlsToCache).then(() => {
-              console.log("All resources cached successfully!");
-          }).catch((err) => {
-              console.error("Resource failed to cache:", err);
-          });
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching all resources");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-      caches.match(event.request).then((response) => {
-          return response || fetch(event.request);
-      })
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse || fetch(event.request).catch(() => caches.match("/"))
+      );
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
